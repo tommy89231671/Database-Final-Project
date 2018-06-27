afa6c0f8d4910c40c30adbe53c99c2aa0036d3cb
DavidHwang7 <40482355+DavidHwang7@users.noreply.github.com>
2018-06-27 08:21:18 -0400

    login asscociated files modify 1

--- db_final_example/auth/login.php
+++ db_final_example/auth/login.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <?php
 	include $_SERVER['DOCUMENT_ROOT'] . '/db_final_example/database/auth.php';
 
@@ -13,10 +14,18 @@
     	header('Location: ' . '/db_final_example/login.php');
 	}
 	// call the class
-	
+	//用sql內建的password函式加密
+	//$password = password($password);
+
 	$auth = new Auth();
 	$login = $auth->login($account, $password);
-	
+	if($login)
+	{
+		$_SESSION['username'] = $account;
+		echo "登入成功!"
+	}	
+	else
+		echo "登入失敗!"
 
 	// redirect to the login.php
 	header('Location: ' . '/db_final_example/home.php');
--- db_final_example/database/auth.php
+++ db_final_example/database/auth.php
@@ -6,7 +6,7 @@
 	// extending the class database/Database makes sure your connection of DB.
 	class Auth extends Database
 	{
-		public function login($account, $password) {
+		public function login($account, $raw_password) {
 			/*
 			#echo $account;
 			#echo $password;
@@ -16,19 +16,22 @@
 			$query1='SELECT * FROM User;';
 			$result=$this->db->query($query1);
 			*/
-			$sql="SELECT Account, Password FROM User WHERE Account=:account AND Password=:password";
+			//$sql="SELECT Account, Password FROM User WHERE Account=:account AND Password=:password";
+			$sql="SELECT Account, user_pass FROM User WHERE Account=:account";
 			$sth =$this->db1->prepare($sql);
-			$sth->execute([':account' => $account, ':password' => $password]);
+			//$sth->execute([':account' => $account, ':password' => $password]);
+			$sth->execute([':account' => $account]);
 
 			if ($sth->rowCount() > 0) {
 				$result=$sth->fetch(PDO::FETCH_OBJ);
-				
+				if(!empty($result)){
+					$hashed_password_from_db = $result['user_pass'];
+					$is_correct_password = password_verify($raw_password, $hashed_password_from_db);
+					return $is_correct_password;
+				}
 			}
-			if(!empty($result->fetchALL())
-
-
 			
-			
+			return 0;
 			
 		}
 	}
